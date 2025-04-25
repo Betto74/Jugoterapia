7c2f223d4f486460c5f8fe93a908c8b0c027eb3b
Betto74 <s21120191@alumnos.itsur.edu.mx>
2025-04-25 12:10:08 -0600

    Modificacion de archivos y responsive

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    const btn = document.querySelector('.hamburger-btn');
-    const menu = document.querySelector('.user-dropdown');
-  
-    btn.addEventListener('click', () => {
-      menu.classList.toggle('active');
-    });
-  
-    // Opcional: cerrar al hacer click fuera
-    document.addEventListener('click', e => {
-      if (!btn.contains(e.target) && !menu.contains(e.target)) {
-        menu.classList.remove('active');
-      }
-    });
+  const btn = document.querySelector('.hamburger-btn');
+  const menu = document.querySelector('.user-dropdown');
+  btn.addEventListener('click', () => {
+    menu.classList.toggle('active');
   });
+  // Cierra al clicar fuera
+  document.addEventListener('click', e => {
+    if (!btn.contains(e.target) && !menu.contains(e.target)) {
+      menu.classList.remove('active');
+    }
+  });
+});
